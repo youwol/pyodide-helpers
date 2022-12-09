75a1cccd0730ab0ee1574a58d26fd9da7e2342d8
youwol-guillaume <greinisch@youwol.com>
2022-12-09 17:42:13 +0100

    🐛 ide-project => fix exports

--- src/lib/ide-project/index.ts
+++ src/lib/ide-project/index.ts
@@ -2,7 +2,10 @@
  * Representation of a project similar to those of an IDE.
  *
  */
-export * as EnvironmentState from './environment.state'
 export * as InWorkerExecutable from './in-worker-executable'
 export * as MainThread from './main-thread'
 export * as WorkersPool from './workers-pool'
+export * from './project-state'
+export * from './models'
+export * from './environment.state'
+export * from './context'
